d65f68a8f90c69708d7232e551f702faa7d786d7
mlesikov <mlesikov@gmail.com>
2017-08-18 12:19:18 +0300

    paying with json
     - parsing object from json
     - post and put form spark kotlin

--- src/main/java/com/mlesikov/AppBootstrap.kt
+++ src/main/java/com/mlesikov/AppBootstrap.kt
@@ -5,8 +5,7 @@ import spark.Filter
 import spark.ResponseTransformer
 import spark.Route
 import spark.Spark.get
-import spark.kotlin.before
-import spark.kotlin.halt
+import spark.kotlin.*
 import spark.servlet.SparkApplication
 
 
@@ -36,12 +35,30 @@ class AppBootstrap : SparkApplication {
     get("/hello") { req, res -> "Hello World" }
 
     get("/json", "application/json", Route({ req, res -> MyMessage("Hello World") }), JsonTransformer())
+
+    get("/json2", "application/json", Route({ req, res ->
+      val message: MyMessage = JsonTransformer().from("{msg: \"parsing test - Hello World\"}", MyMessage::class.java)
+      return@Route message
+    }), JsonTransformer())
+
+    put("/messages/:id", DEFAULT_ACCEPT, {request.params("id")})
+
+    post("/messages/:id", DEFAULT_ACCEPT, {
+      val  m:MyMessage = JsonTransformer().from(request.body(), MyMessage::class.java)
+      MessageRepo().store(m)
+    })
   }
 
 }
 
 
-//  /json
+class MessageRepo {
+  fun store(m: MyMessage){
+  }
+}
+
+
+//  json
 data class MyMessage(val msg: String)
 
 class JsonTransformer : ResponseTransformer {
@@ -52,4 +69,7 @@ class JsonTransformer : ResponseTransformer {
     return gson.toJson(model)
   }
 
+  fun  <T> from(json: String?, clazz: Class<T>): T {
+    return gson.fromJson<T>(json, clazz)
+  }
 }
